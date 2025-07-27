7804052dd920bd70210038596a01b7ced15f4fb9
Haynan Penha <haynan.mesquita@gmail.com>
2025-07-27 11:12:44 -0300

    feat: add Swagger documentation support and enhance user account creation route
    - Added @fastify/swagger and @fastify/swagger-ui dependencies for API documentation.
    - Registered Swagger with basic info and documentation route in server setup.
    - Enhanced create-account route schema with tags and summary for better documentation clarity.
    - Updated Prisma client import path to use generated client.

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,4 +1,6 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
   jsonSchemaTransform,
@@ -12,6 +14,22 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'FullStack SaaS app with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
 
